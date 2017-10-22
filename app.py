d327513514b47c0943b7363a76bc3e299b1f477d
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2017-10-21 20:46:01 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import config
+import os
+import requests
+
+from capitalone import CapitalOne
+
+from flask import Flask, jsonify, request, render_template
+from flask.ext.cors import CORS
+
+c1 = CapitalOne(config.token)
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello, One!'
+
+
+@app.route('/api/one/rewards', methods=['GET'])
+def one_rewards():
+    """ This endpoint takes in an Authorization token and returns the amount of points available in the account. """
+    return jsonify(c1.get_points())
+
+
+@app.route('/api/one/spend', methods=['GET'])
+def one_transactions():
+    return jsonify(c1.get_spend('../transactions/user1.csv'))
+
+if __name__ == '__main__':
+    app.run(debug=True,
+            host='0.0.0.0',
+            port=int(os.environ.get('PORT', 2020)))
---
+++ capitalone.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+import requests
+
+
+class CapitalOne:
+
+    def __init__(self, bearer_token):
+        self.bearer_token = bearer_token
+
+    def get_points(self):
+        http = requests.get('https://api.devexhacks.com/rewards/accounts/', headers={
+                'Authorization': 'Bearer ' + self.bearer_token
+            })
+
+        json = http.json()
+        point_accounts = [
+            account['rewardsAccountReferenceId']
+            for account in json.get('rewardsAccounts', {})
+            if account['rewardsCurrency'] == 'Points'
+        ]
+
+        json = requests.get('https://api.devexhacks.com/rewards/accounts/' + point_accounts[0], headers={
+                    'Authorization': 'Bearer ' + self.bearer_token
+                }).json()
+
+        return json # {'rewardsBalance': json['rewardsBalance']}
+
+    def get_spend(self, transactions_csv):
+        txns = pd.read_csv(transactions_csv)
+        processed = (txns
+            .groupby(['Category', 'SubCategory'])
+            .apply(self._get_stats)
+            .apply(pd.Series)
+            .reset_index())
+
+        processed['pct_spend'] = processed['tot_spend'] / processed['tot_spend'].sum()
+        return processed.to_dict('index')
+
+    def _get_stats(self, group):
+        return {
+            'tot_spend': group['Amount'].sum(),
+            'avg_spend': group['Amount'].mean(),
+            'txn_count': group['Amount'].count()
+        }
